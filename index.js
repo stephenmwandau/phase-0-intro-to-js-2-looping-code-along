6b142c4f5e23a6907875adb3db7c02866fa072dd
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-06-30 13:54:31 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export functions if needed
+  // module.exports = { writeCards, countDown };
+  
